9bd981d32cf8119fb885dcfb1d8a63e1889a0b80
daikinetic <dddaiki.geek@gmail.com>
2023-05-22 16:03:40 +0900

    Initial Commit

---
+++ CollectionViewTrain_v2/CollectionViewTrain_v2App.swift
@@ -0,0 +1,17 @@
+//
+//  CollectionViewTrain_v2App.swift
+//  CollectionViewTrain_v2
+//
+//  Created by Daiki Takano on 2023/05/22.
+//
+
+import SwiftUI
+
+@main
+struct CollectionViewTrain_v2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CollectionViewTrain_v2/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  CollectionViewTrain_v2
+//
+//  Created by Daiki Takano on 2023/05/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
